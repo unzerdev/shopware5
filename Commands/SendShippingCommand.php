1cd236ca302c66c8959db4a28352b7cddeb90885
Niklas Gödde <n.goedde@kellerkinder.de>
2019-08-30 15:11:32 +0200

    [HEIDELPAY-78] Fix command for subshop specific config

--- Commands/SendShippingCommand.php
+++ Commands/SendShippingCommand.php
@@ -2,18 +2,38 @@
 
 namespace HeidelPayment\Commands;
 
+use DateTimeImmutable;
 use Doctrine\DBAL\Connection;
+use HeidelPayment\Services\ConfigReaderServiceInterface;
+use HeidelPayment\Services\HeidelpayApiLoggerServiceInterface;
 use HeidelPayment\Services\ViewBehaviorHandler\ViewBehaviorHandlerInterface;
 use HeidelPayment\Subscribers\Model\OrderSubscriber;
 use heidelpayPHP\Exceptions\HeidelpayApiException;
 use heidelpayPHP\Heidelpay;
 use Shopware\Commands\ShopwareCommand;
-use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class SendShippingCommand extends ShopwareCommand
 {
+    /** @var ConfigReaderServiceInterface */
+    private $configReader;
+
+    /** @var HeidelpayApiLoggerServiceInterface */
+    private $logger;
+
+    /** @var Connection */
+    private $connection;
+
+    public function __construct($name = null, ConfigReaderServiceInterface $configReader = null, HeidelpayApiLoggerServiceInterface $apiLoggerService = null, Connection $connection = null)
+    {
+        $this->configReader = $configReader;
+        $this->logger       = $apiLoggerService;
+        $this->connection   = $connection;
+
+        parent::__construct($name);
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -29,63 +49,62 @@ class SendShippingCommand extends ShopwareCommand
     protected function execute(InputInterface $input, OutputInterface $output): void
     {
         $output->writeln('<comment>Starting automatic shipping notification...</comment>');
-        $configService = $this->container->get('heidel_payment.services.config_reader');
-        $privateKey    = $configService->get('private_key');
-
-        $heidelpayClient = new Heidelpay($privateKey, 'en_GB');
-        $logger          = $this->container->get('heidel_payment.services.api_logger');
 
         $orders = $this->getMatchingOrders();
-
         if (empty($orders)) {
             $output->writeln('<info>No orders where found!</info>');
 
             return;
         }
 
-        $progressBar       = new ProgressBar($output, count($orders));
         $notificationCount = 0;
         foreach ($orders as $order) {
-            $paymentId   = $order['paymentId'];
-            $orderNumber = $order['number'];
-            $orderId     = (int) $order['id'];
-            $invoiceId   = $order['invoiceId'];
+            $paymentId         = $order['paymentId'];
+            $orderNumber       = $order['number'];
+            $orderId           = (int) $order['id'];
+            $invoiceId         = $order['invoiceId'];
+            $shopId            = $order['shopId'];
+            $statusId          = (int) $order['statusId'];
+            $orderStatusConfig = $this->configReader->get('shipping_status', $shopId);
+
+            if (empty($orderStatusConfig) || (int) $orderStatusConfig !== $statusId) {
+                continue;
+            }
+
+            $privateKey      = $this->configReader->get('private_key', $shopId);
+            $heidelpayClient = new Heidelpay($privateKey, 'en_GB');
+
+            $output->writeln(sprintf('Sending shipping notification for order [%s] with payment-id [%s] and invoice-id [%s]...', $orderId, $paymentId, $invoiceId), OutputInterface::VERBOSITY_VERBOSE);
 
             try {
                 $shippingResult = $heidelpayClient->ship($paymentId, $invoiceId);
-                $logger->logResponse(sprintf('Sent shipping notification for order [%s] with payment-id [%s]', $orderNumber, $paymentId), $shippingResult);
+                $this->logger->logResponse(sprintf('Sent shipping notification for order [%s] with payment-id [%s] and invoice id [%s]', $orderNumber, $paymentId, $invoiceId), $shippingResult);
                 $this->updateAttribute($orderId);
 
                 ++$notificationCount;
             } catch (HeidelpayApiException $apiException) {
-                $logger->logException(sprintf('Unable to send shipping notification for order [%s] with payment-id [%s]', $orderNumber, $paymentId), $apiException);
+                $this->logger->logException(sprintf('Unable to send shipping notification for order [%s] with payment-id [%s] and invoice-id [%s]', $orderNumber, $paymentId, $invoiceId), $apiException);
 
-                $output->write(sprintf('<error>Unable to send shipping notification for order [%s] with payment-id [%s]</error>', $orderNumber, $paymentId), true, OutputInterface::VERBOSITY_VERBOSE);
+                $output->writeln(sprintf('<error>Unable to send shipping notification for order [%s] with payment-id [%s] and invoice-id [%s]</error>', $orderNumber, $paymentId, $invoiceId));
             }
-
-            $progressBar->advance();
         }
 
-        $progressBar->clear();
-
-        $output->writeln(sprintf('<info>Automatically sent shipping notification for %s/%s order(s)!</info>', $notificationCount, count($orders)));
+        $output->writeln('');
+        $output->writeln(sprintf('<info>Automatically sent shipping notification for %s order(s)!</info>', $notificationCount));
     }
 
     private function getMatchingOrders()
     {
-        $orderStatusId = (int) $this->container->get('heidel_payment.services.config_reader')->get('shipping_status');
-
-        $queryBuilder = $this->container->get('dbal_connection')->createQueryBuilder();
-        $queryBuilder->select('aOrder.temporaryID AS paymentId, aOrder.ordernumber as number, aDocument.docID as invoiceId, aOrder.id as id')
+        $queryBuilder = $this->connection->createQueryBuilder();
+        $queryBuilder->select('aOrder.temporaryID AS paymentId, aOrder.ordernumber as number, aDocument.docID as invoiceId, aOrder.id as id, aOrder.subshopID as shopId, aOrder.status as statusId')
             ->from('s_order', 'aOrder')
                 ->innerJoin('aOrder', 's_core_paymentmeans', 'aPayment', 'aOrder.paymentID = aPayment.id')
                 ->innerJoin('aOrder', 's_order_attributes', 'aAttribute', 'aOrder.id = aAttribute.orderID')
                 ->leftJoin('aOrder', 's_order_documents', 'aDocument', 'aOrder.id = aDocument.orderID')
             ->where('aPayment.name IN (:paymentMeans)')
                 ->andWhere($queryBuilder->expr()->isNull('aAttribute.heidelpay_shipping_date'))
-                ->andWhere('aOrder.status = :statusId')
+                ->andWhere('aOrder.status != -1')
                 ->andWhere('aDocument.type = :invoiceDocumentType')
-            ->setParameter('statusId', $orderStatusId)
             ->setParameter('invoiceDocumentType', ViewBehaviorHandlerInterface::DOCUMENT_TYPE_INVOICE)
             ->setParameter('paymentMeans', OrderSubscriber::SUPPORTED_PAYMENT_METHOD_NAMES, Connection::PARAM_STR_ARRAY);
 
@@ -94,11 +113,11 @@ class SendShippingCommand extends ShopwareCommand
 
     private function updateAttribute(int $orderId): void
     {
-        $queryBuilder = $this->container->get('dbal_connection')->createQueryBuilder();
+        $queryBuilder = $this->connection->createQueryBuilder();
         $queryBuilder->update('s_order_attributes')
             ->set('heidelpay_shipping_date', ':shippingDate')
             ->where('orderID = :orderId')
-            ->setParameter('shippingDate', (new \DateTimeImmutable())->format('YYYY-dd-mm'))
+            ->setParameter('shippingDate', (new DateTimeImmutable())->format(DATE_ATOM))
             ->setParameter('orderId', $orderId)
             ->execute();
     }
