39e84da85a99202cc49f769d21e5f352445f07ec
Steffen Beisenherz <s.beisenherz@kellerkinder.de>
2021-09-03 14:47:22 +0200

    [HEIDELPAYSUPPORT-151] Prevent editing order positions in unzer orders

--- Resources/views/backend/unzer_payment/app.js
+++ Resources/views/backend/unzer_payment/app.js
@@ -16,6 +16,7 @@
 
 // Views
 //  {include file="backend/unzer_payment/view/detail/unzer.js"}
+//  {include file="backend/unzer_payment/view/detail/position.js"}
 //  {include file="backend/unzer_payment/view/detail/unzer/detail.js"}
 //  {include file="backend/unzer_payment/view/detail/unzer/history.js"}
 //  {include file="backend/unzer_payment/view/detail/unzer/basket.js"}
