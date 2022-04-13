2b7bc6aeaa6deb71f28403d8d3a40f43ec97bdaa
Jan-Erik Spreng <j.spreng@kellerkinder.de>
2022-04-13 11:37:37 +0200

    [HEIDELPAYSUPPORT-226] Add support method to COR-pattern for extending the document view

--- Components/DependencyInjection/Factory/ViewBehavior/ViewBehaviorFactory.php
+++ Components/DependencyInjection/Factory/ViewBehavior/ViewBehaviorFactory.php
@@ -20,6 +20,17 @@ class ViewBehaviorFactory implements ViewBehaviorFactoryInterface
         return $this->viewBehaviorHandler[$paymentName];
     }
 
+    public function getDocumentSupportedBehaviorHandler(string $paymentName, int $documentType): array
+    {
+        if (!array_key_exists($paymentName, $this->viewBehaviorHandler)) {
+            return [];
+        }
+
+        return array_filter($this->viewBehaviorHandler[$paymentName], static function(ViewBehaviorHandlerInterface $behaviorHandler) use($documentType) {
+            return $behaviorHandler->supportDocumentBehavior($documentType);
+        });
+    }
+
     public function addBehaviorHandler(ViewBehaviorHandlerInterface $viewBehaviorHandler, string $paymentName): void
     {
         $this->viewBehaviorHandler[$paymentName][] = $viewBehaviorHandler;
