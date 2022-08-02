b4b9e3b3541ff5c65f663679c31a6f5e598c5452
mkreusch <info@kreusch-creative.de>
2022-08-02 14:29:16 +0200

    bug fix - column type json

--- Installers/Assets/sql/update.sql
+++ Installers/Assets/sql/update.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `s_plugin_unzer_order_ext_backup` (
     `unzer_order_id` varchar(50) NOT NULL,
     `payment_name` varchar(50) NOT NULL,
-    `user_data` json NOT NULL,
-    `basket_data` json NOT NULL,
+    `user_data` longtext NOT NULL,
+    `basket_data` longtext NOT NULL,
     `s_comment` longtext NULL,
     `dispatch_id` int(11) NOT NULL,
     `created_at` datetime NOT NULL ON UPDATE now()
