0dbd1121b84db6a433839c2cca5642378758c1e3
Timo Helmke <t.helmke@kellerkinder.de>
2023-06-16 10:36:50 +0200

    feat: [HEIDELPAYSUPPORT-207] Optimize database creating. Remove certificates on uninstall.

--- Installers/Assets/sql/uninstall.sql
+++ Installers/Assets/sql/uninstall.sql
@@ -1,2 +1,3 @@
 DROP TABLE IF EXISTS s_plugin_unzer_payment_vault;
 DROP TABLE IF EXISTS s_plugin_unzer_order_ext_backup;
+DROP TABLE IF EXISTS s_plugin_unzer_apple_pay_configuration;
---
+++ Installers/Certificates.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace UnzerPayment\Installers;
+
+use Doctrine\DBAL\Connection;
+use League\Flysystem\FilesystemInterface;
+use UnzerPayment\Components\ApplePay\CertificateManager;
+
+class Certificates
+{
+    /** @var Connection */
+    private $connection;
+
+    /** @var FilesystemInterface */
+    private $filesystem;
+
+    /** @var CertificateManager */
+    private $certificateManager;
+
+    public function __construct(Connection $connection, FilesystemInterface $filesystem)
+    {
+        $this->connection = $connection;
+        $this->filesystem = $filesystem;
+        $this->certificateManager = new CertificateManager($connection);
+    }
+
+    public function uninstall(): void
+    {
+        $shops = $this->connection->fetchAllAssociative('SELECT id FROM `s_core_shops`');
+
+        foreach ($shops as $shop) {
+            $shopId = (int) $shop['id'];
+            $certficatePath = $this->certificateManager->getMerchantIdentificationCertificatePath($shopId);
+            if ($this->filesystem->has($certficatePath)) {
+                $this->filesystem->delete($certficatePath);
+            }
+
+            $keyPath = $this->certificateManager->getMerchantIdentificationKeyPath($shopId);
+            if ($this->filesystem->has($keyPath)) {
+                $this->filesystem->delete($keyPath);
+            }
+        }
+    }
+}
