d717bd25cc04c51e8ddd5eebc0e112547f78eec4
Dominik Wißler <d.wissler@kellerkinder.de>
2024-01-26 15:38:57 +0100

    fix: [HEIDELPAYSUPPORT-340] Fix calculation of remaining amount in canceling charges in backend

--- Controllers/Backend/UnzerPayment.php
+++ Controllers/Backend/UnzerPayment.php
@@ -171,7 +171,10 @@ class Shopware_Controllers_Backend_UnzerPayment extends Shopware_Controllers_Bac
                 case 'charge':
                     /** @var Charge $transactionResult */
                     $transactionResult = $payment->getCharge($transactionId);
-                    $remainingAmount   = $transactionResult->getAmount() - $transactionResult->getCancelledAmount();
+                    $remainingAmount = $transactionResult->getAmount();
+                    foreach ($transactionResult->getPayment()->getCancellations() as $cancellation) {
+                        $remainingAmount -= $cancellation->getAmount() ?? 0;
+                    }
 
                     break;
 
